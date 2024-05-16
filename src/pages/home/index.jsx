76d58489c446d41d833bc05d5ce258e2c95d7965
Shubham-Lal <im.shubhamlal@gmail.com>
2024-05-16 12:44:59 +0530

    fix width sizing

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,7 @@
 import "./style.css";
+import { Link } from "react-router-dom";
 import Lottie from "lottie-react";
 import Welcome from "../../lottie/Welcome.json";
-import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
