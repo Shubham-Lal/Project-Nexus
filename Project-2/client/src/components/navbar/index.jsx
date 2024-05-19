f428ad2323549723f97f8890b63d75cdc9f5758f
Shubham-Lal <im.shubhamlal@gmail.com>
2024-05-19 13:54:07 +0530

    Project-2: create Navbar

--- Project-2/client/src/components/navbar/index.jsx
+++ Project-2/client/src/components/navbar/index.jsx
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom';
 import './style.css';
 
 const Navbar = () => {
     return (
         <nav>
+            <h1>Pizzeria</h1>
 
+            <img src='/arrow.svg' alt='Arrow' />
+
+            <div className='links__container'>
+                <Link to='/'>Order Now</Link>
+                <Link to='/login'>Login</Link>
+            </div>
         </nav>
     )
 }
